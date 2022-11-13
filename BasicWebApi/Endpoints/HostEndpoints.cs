31357a94f535e048bf7a82f40c142ff9b22dc841
Mitchell Barry <mitch.barry@gmail.com>
2022-11-12 21:30:35 -0600

    Fixed whitespace

--- BasicWebApi/Endpoints/HostEndpoints.cs
+++ BasicWebApi/Endpoints/HostEndpoints.cs
@@ -3,42 +3,57 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BasicWebApi.Endpoints
 {
-	public static class HostEndpoints
-	{
-		public static RouteGroupBuilder MapHostInfoApi(this RouteGroupBuilder group)
-		{
-			group.MapGet("/", GetHostInfo);
-            group.MapGet("/2", GetHostInfo2)
-				.WithOpenApi(operation => new (operation)
-				{
-					Deprecated = true
-				});
+    public static class HostEndpoints
+    {
+        public static RouteGroupBuilder
+        MapHostInfoApi(this RouteGroupBuilder group)
+        {
+            group.MapGet("/", GetHostInfo);
+            group
+                .MapGet("/2", GetHostInfo2)
+                .WithOpenApi(operation =>
+                    new (operation) { Deprecated = true });
             return group;
-		}
+        }
 
-		[EndpointName("GetHostInfo")]
-		[EndpointSummary("Returns host information to any client")]
-		[EndpointDescription("Example endpoint which returns simple host information")]
-		[Produces(typeof(HostInfo))]
-		public static async Task<HostInfo> GetHostInfo([FromServices] IHttpContextAccessor httpContextAccessor)
-		{
-			return new HostInfo
-			{
-				HostIp = httpContextAccessor?.HttpContext?.Connection?.LocalIpAddress?.ToString()
+        [EndpointName("GetHostInfo")]
+        [EndpointSummary("Returns host information to any client")]
+        [
+            EndpointDescription(
+                "Example endpoint which returns simple host information")
+        ]
+        [Produces(typeof (HostInfo))]
+        public static async Task<HostInfo>
+        GetHostInfo([FromServices] IHttpContextAccessor httpContextAccessor)
+        {
+            return new HostInfo {
+                HostIp =
+                    httpContextAccessor?
+                        .HttpContext?
+                        .Connection?
+                        .LocalIpAddress?
+                        .ToString()
             };
-		}
+        }
 
         [EndpointName("GetHostInfo2")]
         [EndpointSummary("Returns host information to any client")]
-        [EndpointDescription("Example endpoint which returns simple host information")]
-        [Produces(typeof(HostInfo))]
-        public static async Task<HostInfo> GetHostInfo2([FromServices] IHttpContextAccessor httpContextAccessor)
+        [
+            EndpointDescription(
+                "Example endpoint which returns simple host information")
+        ]
+        [Produces(typeof (HostInfo))]
+        public static async Task<HostInfo>
+        GetHostInfo2([FromServices] IHttpContextAccessor httpContextAccessor)
         {
-            return new HostInfo
-            {
-                HostIp = httpContextAccessor?.HttpContext?.Connection?.LocalIpAddress?.ToString()
+            return new HostInfo {
+                HostIp =
+                    httpContextAccessor?
+                        .HttpContext?
+                        .Connection?
+                        .LocalIpAddress?
+                        .ToString()
             };
         }
     }
 }
-
--- BasicWebApi/Models/HostInfo.cs
+++ BasicWebApi/Models/HostInfo.cs
@@ -5,4 +5,3 @@
         public string HostIp { get; set; }
     }
 }
-
